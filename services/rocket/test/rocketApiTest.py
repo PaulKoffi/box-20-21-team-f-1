003974ce1fd17381cffe1e01301f1387cc46d7c6
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-10-04 23:48:37 +0200

    #14 nouvel essai

--- services/rocket/test/rocketApiTest.py
+++ services/rocket/test/rocketApiTest.py
@@ -1,7 +1,7 @@
 # # rocketApiTest.py
 
-import sys, os
-sys.path.append(os.path.abspath(os.path.join('..', 'box-20-21-team-f/services/rocket/src')))
+# import sys, os
+# sys.path.append(os.path.abspath(os.path.join('..', 'box-20-21-team-f/services/rocket/src')))
 
 from run import app
 import unittest
