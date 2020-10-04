1ab96dea185f41afb694f0bca93b8e0ef96814b5
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-10-04 21:32:11 +0200

    #12 Modification du dictionnairedans payloadResource

--- services/payload/src/server/resources/payloadResource.py
+++ services/payload/src/server/resources/payloadResource.py
@@ -2,18 +2,21 @@ from flask import jsonify
 
 payloads = {
     "satelite1": {
+        "idpayload" : "satellite1",
         "position": {"x": 1.0, "y":1.0},
         "vitesse": 30.0,
         "customer": {"name":"Paul-Marie"},
         "rocketName": "Arianne-240",
     },
     "satelite2": {
+        "idpayload" : "satellite2",
         "position": {"x": 1.0, "y":1.0},
         "vitesse": 30.0,
         "customer": {"name":"Paul-Marie"},
         "rocketName": "Arianne-2800",
     },
     "satelite3": {
+        "idpayload" : "satellite3",
         "position": {"x": 1.0, "y":1.0},
         "vitesse": 30.0,
         "customer": {"name":"Paul-Marie"},
