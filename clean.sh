884d9a4d759280fbb900d18e45934994acb61808
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-10-06 23:50:10 +0200

    #14 mise en commentaire de la ligne docker rmi $...

--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@
 ## Removing all images
 docker rmi djotiham/richardcli djotiham/torycli djotiham/eloncli djotiham/pollcreator_rpc djotiham/rocket_inventory_service djotiham/weather_service
 ## Removing old/dandling images
-docker rmi $(docker images -qa -f 'dangling=true')
+# docker rmi $(docker images -qa -f 'dangling=true')
 
 # echo "Done"
 # read -n 1 -s -r -p "Press any key to continue"
