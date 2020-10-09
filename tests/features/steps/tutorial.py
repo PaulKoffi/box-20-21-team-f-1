10b79534390ac53e2e2d40e0f743fff619ad3c6c
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-10-09 20:08:49 +0200

    #18 changement des adresses 0.0.0.0 en localhost

--- tests/features/steps/tutorial.py
+++ tests/features/steps/tutorial.py
@@ -11,3 +11,15 @@
 # @then('behave will test it for us!')
 # def step_impl(context):
 #     assert context.failed is False
+
+
+# lancer rpc/rocket et rpc/socketServer
+# lancer jeffDashBoard/client
+# lancer services/launcher 
+
+# lancer la commande sendStates dans elon 
+
+# lancer startpoll de richard 
+
+# dnspython >= 2.0.0
+# pymongo >= 3.11.0
