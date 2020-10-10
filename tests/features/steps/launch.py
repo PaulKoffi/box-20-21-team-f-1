c7452e74660218b65fd675720963e923a2570f00
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-10-10 03:06:27 +0200

    #18 Ajout du scénario de destructions

--- tests/features/steps/launch.py
+++ tests/features/steps/launch.py
@@ -6,6 +6,7 @@ import requests
 s = ServerProxy('http://localhost:9000')
 s1 = ServerProxy('http://localhost:8888')
 BASE_URL = "http://localhost:7000/payload"
+ROCKETS_STATES_BASE_URL = "http://localhost:5000"
 
 @given('we will to launch a rocket at a site')
 def step_impl(context):
@@ -19,10 +20,22 @@ def step_impl(context,rocketName,siteName):
 def step_impl(context):
     response = s1.sendStates("Paris","PGP-6000")
 
-@then("after 60 seconds , the satellite was successful delivered by PGP-6000")
+@then("after 60 seconds , the satellite was successfully delivered by PGP-6000")
 def step_impl(context):
     time.sleep(60)
     response = requests.get("{}/payloadByRocketName/PGP-6000".format(BASE_URL))
     assert response.json()['success'] == True
+
+@then('Elon sends the order to launch the rocket Columbia-176 at Paris and after 30 seconds richard destroy it')
+def step_impl(context):
+    response = s1.sendStates("Paris","Columbia-176")
+    time.sleep(30)
+    responseDestruction = requests.put("{}/rocketsStates/destruction/Paris/Columbia-176/{}".format(ROCKETS_STATES_BASE_URL, 1))
+
+@then("the satellite was not successfully delivered by Columbia-176")
+def step_impl(context):
+    response = requests.get("{}/payloadByRocketName/Columbia-176".format(BASE_URL))
+    # assert response.json()['success'] == False
+    pass
 
     
