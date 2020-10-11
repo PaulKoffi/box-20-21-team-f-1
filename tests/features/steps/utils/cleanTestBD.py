158db6ade2131b28fe831ee2bf9af91886ea32c1
PaulKoffi <merveille.koffi@etu.unice.fr>
2020-10-11 21:04:21 +0200

    #9 cleanBD

--- tests/features/steps/utils/cleanTestBD.py
+++ tests/features/steps/utils/cleanTestBD.py
@@ -1,6 +1,5 @@
 import pymongo
 
-
 client = pymongo.MongoClient(
     "mongodb+srv://flo:Azerty123@cluster0.ibhol.mongodb.net/blueOrigin?retryWrites=true&w=majority")
 db = client.get_database('blueOrigin')
@@ -8,12 +7,11 @@ db = client.get_database('blueOrigin')
 myquery = {"rocketName": "VEGA-4000"}
 newvalues = {"$set": {"launch": False, "secondStep": False}}
 db.rocketActions.update_one(myquery, newvalues)
-db.payloads.delete_one({"satellite": "CORSAIRE"})
-db.rocketinventories.delete_one({"rocketName": "VEGA-4000"})
-
+db.payloads.delete_many({"satellite": "CORSAIRE"})
+db.rocketinventories.delete_many({"rocketName": "VEGA-4000"})
 
 myquery = {"rocketName": "SOUL-9000"}
 newvalues = {"$set": {"launch": False, "secondStep": False}}
 db.rocketActions.update_one(myquery, newvalues)
-db.payloads.delete_one({"satellite": "PERSEUS"})
-db.rocketinventories.delete_one({"rocketName": "SOUL-9000"})
+db.payloads.delete_many({"satellite": "PERSEUS"})
+db.rocketinventories.delete_many({"rocketName": "SOUL-9000"})
