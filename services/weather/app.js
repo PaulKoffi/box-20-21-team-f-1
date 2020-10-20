e044d4e54323aa3669f5abcc864e5cbd5aa29d26
PaulKoffi <merveille.koffi@etu.unice.fr>
2020-10-20 20:36:46 +0200

    #34 ajout d'un producer et consumer dans weatherService pour la réponse du poll

--- services/weather/app.js
+++ services/weather/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const logger = require('koa-logger');
 const config = require('./config');
 const sdk = require('./src/server/sdk');
-
+const { Kafka } = require('kafkajs');
 
 const siteRoute  = require('./src/server/route/site');
 
@@ -28,6 +28,55 @@ const server = app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
 });
 
+                                                 //  ######### KAFKA ######## //
+const kafka = new Kafka({
+    clientId: 'weather-node',
+    brokers: ['localhost:9092']
+});
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: 'weather-group' });
+
+const run = async () => {
+    // Producing
+    await producer.connect();
+
+    // Consuming
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'Pollrequesttopic', fromBeginning: true });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log("ici");
+            console.log({
+                partition,
+                offset: message.offset,
+                value: message.value.toString()
+            });
+
+
+            if (message.value.toString() === "sites") {
+                const weather = await sdk.getSite();
+                await producer.send({
+                    topic: 'pollresponsetopic',
+                    messages: [
+                        { value: JSON.stringify(weather) },
+                    ],
+                });
+            } else {
+                const weather = await sdk.getSiteByName(message.value.toString());
+                await producer.send({
+                    topic: 'pollresponsetopic',
+                    messages: [
+                        { value: JSON.stringify(weather) },
+                    ],
+                });
+            }
+        },
+    })
+};
+
+run().catch(console.error);
 
 module.exports = {
     server
