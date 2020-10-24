ef99590b34a1eb626d3093226fe8cbc11e56f5d1
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-10-24 19:57:57 +0200

    dockerisation du delivery service

--- docker/services/delivery/build.sh
+++ docker/services/delivery/build.sh
@@ -8,5 +8,3 @@ docker build -t djotiham/delivery_service .
 rm -R resources
 # to remove old images
 # docker rmi $(docker images -qa -f 'dangling=true')
-# echo "Done"
-# read -n 1 -s -r -p "Press any key to continue"
--- docker/services/delivery/run.sh
+++ docker/services/delivery/run.sh
@@ -5,7 +5,6 @@
 #  - detached (-d),
 #  - binding localhost:7000 to container:7000
 docker run --name delivery_service --rm -d -p 7000:7000 djotiham/delivery_service
-
 # to stop: docker stop ID
 # to start a new shell in the container: docker exec -it ID bash
 # to attach to the container: docker attach ID (^P ^Q to detach)
