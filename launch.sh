dac53cd426cd533596653fee4cd295eeab6e116b
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-11-01 10:46:22 +0100

    Suppression des fichiers .sh inutiles

--- launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## >>>>> Running all servers
-cd docker
-docker-compose up -d
-
-# echo "Done"
-# read -n 1 -s -r -p "Press any key to continue"
--- run.sh
+++
@@ -1,3 +0,0 @@
-echo "launching scenarios ..."
-
-./tests.sh
