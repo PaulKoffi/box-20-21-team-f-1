1d25e79b129427f61ee78d5779e17f736d94e45e
PaulKoffi <merveille.koffi@etu.unice.fr>
2020-11-11 21:19:33 +0100

    #43 add github actions CI

--- prepare.sh
+++ prepare.sh
@@ -199,7 +199,7 @@ nohup ./triggerAnomaly.sh &
 
 nohup ./weather.sh &
 
-docker exec -it -T tests ./run.sh
+docker exec -it tests ./run.sh
 export COMPOSE_INTERACTIVE_NO_CLI=1
 #nohup ./launcher.sh &
 #read -n 1 -s -r -p "Press any key to continue"
